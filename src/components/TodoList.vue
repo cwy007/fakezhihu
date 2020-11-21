f039807831a83e798cba3b09dae6160b275126fb
chenweiyan <chanweiyan007@gmail.com>
2020-11-22 03:35:44 +0800

    todo list

---
+++ src/components/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="input-part">
+      <input v-model="words" type="text" />
+      <button v-on:click="insertItem">插入</button>
+    </div>
+    <ul>
+      <li v-for="(item, index) in list" v-bind:key="index">
+        <TodoListItem
+          v-bind:item="item"
+          v-bind:index="index"
+          v-on:delete-item="deleteItem"
+          v-on:modify-item="modifyItem"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoListItem from "./TodoListItem";
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      words: " ",
+      list: []
+    };
+  },
+  components: { TodoListItem },
+  methods: {
+    insertItem() {
+      this.list.push(this.words);
+    },
+    deleteItem(index) {
+      this.list.splice(index, 1);
+    },
+    modifyItem(newContent, index) {
+      this.list.splice(index, 1, newContent);
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TodoList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TodoList from "@/components/TodoList.vue";
+import "@/assets/style.css";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    TodoList
   }
 };
 </script>
+
+<style lang="css">
+.home {
+  margin: 0 auto;
+  text-align: center;
+  font-size: 12.077rpx;
+}
+</style>
